bfcb6abe8fe1c05237e572f9443cfb452a6f2c37
YTolibRakhmonov <t.rakhmonov@student.inha.uz>
2019-07-27 15:58:13 +0500

    sdsdsdsds

--- src/components/logout.vue
+++ src/components/logout.vue
@@ -1,76 +1,28 @@
 <template>
-    <v-app>
-         <v-container
-        fluid
-        fill-height
-      >
-        <v-layout
-          align-center
-          justify-center
-        >
-          <v-flex
-            xs12
-            sm8
-            md4
-          >
-            <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>Login</v-toolbar-title>
-                <v-spacer></v-spacer>
-                
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="E-mail"
-                    name="email"
-                    prepend-icon="person"
-                    type="text"
-                    v-model="email"
-                  ></v-text-field>
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    prepend-icon="lock"
-                    type="password"
-                    v-model="password"
-                  ></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" @click="login">Login</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <v-flex xs12 sm8 md4>
-          <h3>You are logged out</h3>
-      </v-flex>
-    </v-app>
+  <v-app>
+    <v-container  fill-height>
+      <v-layout row wrap align-center>
+        <v-flex text-center>
+          <h1>You are logged out</h1>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
-data(){
-    return{
-        email: null,
-        password: null
-    }
-},
-created(){
-    localStorage.removeItem('token');
-}
-}
+  data() {
+    return {
+      email: null,
+      password: null
+    };
+  },
+  created() {
+    localStorage.removeItem("token");
+  }
+};
 </script>
 
 <style>
-
 </style>
