cf9aad37a4d1c0253391d68580faddce4b3f3dd8
YTolibRakhmonov <t.rakhmonov@student.inha.uz>
2019-07-29 14:37:42 +0500

    dsfsd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,12 @@
       <!-- swiper -->
       <swiper :options="swiperOption">
         <swiper-slide>
-          <img class="img-fluid" src="@/assets/imgs/1.jpg" alt="Slider image" />
+          <img class="img-fluid" src="@/assets/imgs/slide1.jpg" alt="Slider image" />
         </swiper-slide>
         <swiper-slide>
-          <img class="img-fluid" src="@/assets/imgs/2.jpg" alt="Slider image" />
+          <img class="img-fluid" src="@/assets/imgs/slide2.jpeg" alt="Slider image" />
         </swiper-slide>
-        <swiper-slide>
+        <!-- <swiper-slide>
           <img class="img-fluid" src="@/assets/imgs/3.jpg" alt="Slider image" />
         </swiper-slide>
         <swiper-slide>
@@ -18,7 +18,7 @@
         </swiper-slide>
         <swiper-slide>
           <img class="img-fluid" src="@/assets/imgs/5.jpg" alt="Slider image" />
-        </swiper-slide>
+        </swiper-slide> -->
         <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div>
@@ -57,10 +57,15 @@ export default {
 </script>
 <style scoped>
 .slider {
+  padding-bottom: 0;
+  max-width: 90%;
+  max-height: 100%; 
   background-repeat: no-repeat;
 }
 .startup_process {
   background-color: rgb(234, 244, 248);
+  max-width: 90%;
+  max-height: 100%;
 }
 </style>
 
