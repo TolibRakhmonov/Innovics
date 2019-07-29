ac90ba6a715e4742fc30862aea1692e44c6691e0
YTolibRakhmonov <t.rakhmonov@student.inha.uz>
2019-07-29 16:49:40 +0500

    card started

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,15 +10,6 @@
         <swiper-slide>
           <img class="img-fluid" src="@/assets/imgs/slide2.jpeg" alt="Slider image" />
         </swiper-slide>
-        <!-- <swiper-slide>
-          <img class="img-fluid" src="@/assets/imgs/3.jpg" alt="Slider image" />
-        </swiper-slide>
-        <swiper-slide>
-          <img class="img-fluid" src="@/assets/imgs/4.jpg" alt="Slider image" />
-        </swiper-slide>
-        <swiper-slide>
-          <img class="img-fluid" src="@/assets/imgs/5.jpg" alt="Slider image" />
-        </swiper-slide> -->
         <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div>
@@ -26,6 +17,25 @@
     </section>
     <section class="container startup_process">
       <img class="img-fluid" src="@/assets/imgs/startup_process.png" alt="Image" />
+    </section>
+    <section class="successful_startups container">
+      <div class="text-center">
+        <h1>Successful Startups</h1>
+      </div>
+      <div class="d-flex flex-wrap align-content-center">
+        <div class="flex_item">
+          <img src="@/assets/imgs/1.jpg" alt="Image" />
+        </div>
+        <div class="flex_item">
+          <img src="@/assets/imgs/2.jpg" alt="Image" />
+        </div>
+        <div class="flex_item">
+          <img src="@/assets/imgs/3.jpg" alt="Image" />
+        </div>
+        <div class="flex_item">
+          <img src="@/assets/imgs/4.jpg" alt="Image" />
+        </div>
+      </div>
     </section>
   </v-app>
 </template>
@@ -59,13 +69,27 @@ export default {
 .slider {
   padding-bottom: 0;
   max-width: 90%;
-  max-height: 100%; 
+  max-height: 100%;
   background-repeat: no-repeat;
 }
 .startup_process {
   background-color: rgb(234, 244, 248);
   max-width: 90%;
   max-height: 100%;
+  margin-top: 30px;
 }
+.successful_startups {
+  max-width: 90%;
+  max-height: 100%;
+  margin-top: 30px;
+}
+.flex_item{
+  margin: 10px;
+  
+}
+.flex_item img{
+  border-radius: 5px;
+}
+
 </style>
 
